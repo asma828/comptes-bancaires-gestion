eb5c2e94e51138cd90856c705f309206e913e928
asma828 <asmalachhab66@gmail.com>
2025-09-17 17:12:46 +0100

    creation de la methode creatAccount and checkSolde

--- untitled/src/Controller/BanqueController.java
+++ untitled/src/Controller/BanqueController.java
@@ -1,4 +1,48 @@
 package Controller;
 
+import Model.Compte;
+import Model.CompteCourant;
+import Model.CompteEpargne;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class BanqueController {
+    private static Scanner scanner = new Scanner(System.in);
+    private static HashMap<String, Compte> comptes = new HashMap<>();
+
+    public static void createAccount() {
+        System.out.println("\n ** CREATE ACCOUNT **");
+        System.out.println("1. Compte d'épargne");
+        System.out.println("2. Compte courant");
+        System.out.println("3. Retour au menu principal");
+
+        int type = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (type) {
+            case 1:
+                System.out.println("entrer le code du compte :");
+                String codeEpagne = scanner.nextLine();
+                comptes.put(codeEpagne, new CompteEpargne(codeEpagne, 0));
+                System.out.println("compte creé avec succes");
+                break;
+
+            case 2:
+                System.out.println("entrer le code du compte :");
+                String codeCourant = scanner.nextLine();
+                comptes.put(codeCourant,new CompteCourant(codeCourant,0));
+                System.out.println("compte creé avec succes");
+        }
+
+
+    }
+public  static void checkSolde(){
+        System.out.println("entre le code du compte");
+        String code = scanner.nextLine();
+        Compte compte = comptes.get(code);
+        if(compte != null){
+            System.out.println("votre solde est " + compte.getSolde());
+        }
+}
 }
