3d4c65640a1c45cc7a43ae0f87cc3fb21d8b2758
asma828 <asmalachhab66@gmail.com>
2025-09-19 15:42:09 +0100

    ajouter les getters pour la class Versement

--- untitled/src/Model/Versement.java
+++ untitled/src/Model/Versement.java
@@ -1,4 +1,12 @@
 package Model;
 
 public class Versement extends Operation {
+    private String source;
+
+    public Versement(String numero, String date, double montant, String source) {
+        super(numero, date, montant);
+        this.source = source;
+    }
+
+    public String getSource() { return source; }
 }
