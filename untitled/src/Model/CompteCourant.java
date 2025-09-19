b493c6e94611d60b2f3e4e679348b34b5abac566
asma828 <asmalachhab66@gmail.com>
2025-09-19 15:35:11 +0100

    redefini les methodes de la class mére compte

--- untitled/src/Model/CompteCourant.java
+++ untitled/src/Model/CompteCourant.java
@@ -1,11 +1,36 @@
 package Model;
 
-public class CompteCourant extends Compte {
-    private float decouvert;
+import java.time.LocalDate;
+import java.util.UUID;
 
-    public CompteCourant(String code, float decouvert) {
+public class CompteCourant extends Compte {
+    private double decouvert;
+
+    public CompteCourant(String code, double decouvert) {
         super(code);
-        this.code = code;
         this.decouvert = decouvert;
+    }
+
+    @Override
+    public boolean retirer(double montant, String destination) {
+        if (solde - montant >= -decouvert) {
+            solde -= montant;
+            listeOperations.add(new Retrait(UUID.randomUUID().toString(),
+                    LocalDate.now().toString(), montant, destination));
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public double calculerInteret() {
+        return 0;
+    }
+
+    @Override
+    public void afficherDetails() {
+        System.out.println("Compte Courant: " + code);
+        System.out.println("Solde: " + solde);
+        System.out.println("Découvert: " + decouvert);
     }
 }
