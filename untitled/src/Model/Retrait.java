f73cf60b030cfa0d8869dbbef8743ed1a7ba3f0e
asma828 <asmalachhab66@gmail.com>
2025-09-19 15:40:59 +0100

    ajouter les getters pour la class Retrait

--- untitled/src/Model/Retrait.java
+++ untitled/src/Model/Retrait.java
@@ -1,4 +1,12 @@
 package Model;
 
 public class Retrait extends Operation {
+    private String destination;
+
+    public Retrait(String numero, String date, double montant, String destination) {
+        super(numero, date, montant);
+        this.destination = destination;
+    }
+
+    public String getDestination() { return destination; }
 }
