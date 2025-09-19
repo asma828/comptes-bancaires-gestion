6e71f40d32e9f7f6583cc7655166febabb02812e
asma828 <asmalachhab66@gmail.com>
2025-09-19 15:36:38 +0100

    redefini les methodes de la class mére compte

--- untitled/src/Model/CompteEpargne.java
+++ untitled/src/Model/CompteEpargne.java
@@ -1,11 +1,36 @@
 package Model;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class CompteEpargne extends Compte {
     private double tauxInteret;
 
-    public CompteEpargne(String code ,double tauxInteret){
+    public CompteEpargne(String code, double tauxInteret) {
         super(code);
-        this.code=code;
-        this.tauxInteret=tauxInteret;
+        this.tauxInteret = tauxInteret;
+    }
+
+    @Override
+    public boolean retirer(double montant, String destination) {
+        if (solde >= montant) {
+            solde -= montant;
+            listeOperations.add(new Retrait(UUID.randomUUID().toString(),
+                    LocalDate.now().toString(), montant, destination));
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public double calculerInteret() {
+        return solde * tauxInteret;
+    }
+
+    @Override
+    public void afficherDetails() {
+        System.out.println("Compte Epargne: " + code);
+        System.out.println("Solde: " + solde);
+        System.out.println("Taux: " + tauxInteret);
     }
 }
